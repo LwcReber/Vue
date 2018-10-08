3b41ac65814592cb6c00b34fc0c5cc91635da42d
LwcReber <1079670310@qq.com>
2018-10-08 23:46:56 +0800

    添加server

--- vue-ssr/vue-ssr-practice2/server/routers/dev-ssr.js
+++ vue-ssr/vue-ssr-practice2/server/routers/dev-ssr.js
@@ -1,62 +1,45 @@
-// 开发环境处理
-
 const Router = require('koa-router')
+const axios = require('axios')
 const path = require('path')
 const fs = require('fs')
-let axios = require('axios')
-const MemoryFS = require('memory-fs') // 不把文件写入到磁盘，直接写入内存， 可以提高效率
+const MemoryFs = require('memory-fs') // 不把文件写入磁盘，直接写入内存中，读取文件和输出文件较快
 const webpack = require('webpack')
-const VuerServerRenderer = require('vue-server-renderer')
-const serverRender = require('./server-render')
+const VueServerRenderer = require('vue-server-renderer')
+
 const serverConfig = require('../../build/webpack.config.server')
 
 const serverCompiler = webpack(serverConfig)
-const mfs = new MemoryFS()
-
+const mfs = new MemoryFs()
 serverCompiler.outputFileSystem = mfs
 
 let bundle
-serverCompiler.watch({}, (err, stats) => {
+serverCompiler.watch({}, (err,stats) => {
   if(err) throw err
-  // webpack 打包报错提醒
   stats = stats.toJson()
-  stats.errors.forEach(err => console.log(err))
-  stats.warnings.forEach(warn => console.warn(err))
+  stats.errors.forEach((err) => console.log(err))
+  stats.warnings.forEach(warn => console.warn(warn))
 
   const bundlePath = path.join(
     serverConfig.output.path,
     'vue-ssr-server-bundle.json'
   )
+
   bundle = JSON.parse(mfs.readFileSync(bundlePath, 'utf-8'))
-  console.log('new bundle generated');
 })
 
-const handleSSR = async  (ctx) => {
-  // 有包的时候才启动， 防止刚启动时，webpack还没有打包好导致报错
-  if(!bundle) {
+const handleSSR = async (ctx) => {
+  if (!bundle) {
     ctx.body = '等会，别急'
     return
   }
-
-  let clientManifestResp = await axios.get('http://127.0.0.1:8000/public/vue-ssr-client-manifest.json')
-
-  const clientManifest = clientManifestResp.data
-
   const template = fs.readFileSync(
-    path.join(__dirname, '../server.template.ejs'),
-    'utf-8' // 指定为utf-8格式才能是字符串
+    path.join(__dirname, '../server.template.ejs')
   )
-  const renderer = VuerServerRenderer
+
+  const renderer = VueServerRenderer
     .createBundleRenderer(bundle, {
-      inject: false, //  不使用官方的固定模板来注入，自己处理
-      clientManifest
+      inject: false,
+      
     })
 
-    await serverRender(ctx, renderer, template)
 }
-
-const router = new Router()
-
-router.get('*', handleSSR)
-
-module.exports = router
--- vue-ssr/vue-ssr-practice2/server/server.js
+++ vue-ssr/vue-ssr-practice2/server/server.js
@@ -1,51 +1,22 @@
-const Koa = require('koa');
-const send = require('koa-send')
-const app = new Koa()
-const path = require('path')
+const Koa = require('koa')
 
-const staticRouter = require('./routers/static')
+const  app = new koa()
 
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV  === 'development' // 服务端渲染需要区分开发环境或者生产环境，不同环境差别大
 
+
+// 中间件 可以拍查错误
 app.use(async (ctx, next) => {
   try {
-    console.log(`rquest wiith path ${ctx.path}`);
+    console.log(`request with path ${ctx.path}`);
     await next()
   } catch (err) {
     console.log(err);
-    ctx.status == 500
-    if (isDev) {
+    ctx.status = 500;
+    if(isDev) {
       ctx.body = err.message
     } else {
       ctx.body = 'please try again later'
     }
   }
-})
-app.use(async (ctx, next) => {
-  if(ctx.path == '/favicon.ico') {
-    await send(ctx, '/favicon.ico', {root: path.join(__dirname, '../')})
-  } else {
-    await next()
-  }
-})
-
-
-app.use(staticRouter.routes()).use(staticRouter.allowedMethods())
-
-
-let pageRouter
-if(isDev) {
-  pageRouter = require('./routers/dev-ssr')
-} else {
-    pageRouter = require('./routers/ssr')
-}
-app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
-
-
-
-const HOST = process.env.HOST || '0.0.0.0'
-const PORT = process.env.PORT || 3333
-
-app.listen(PORT, HOST, () => {
-  console.log(`server is listening on ${HOST}:${PORT}`);
 })
